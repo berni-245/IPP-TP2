eea35672676f7b87c99d59311fb644470243c93e
berni <bernizapico@gmail.com>
2025-09-09 15:28:53 -0300

    feat: added forkJoin parallel version for matrix multiplication

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,36 @@
 import matrix.MatrixMultiplication;
 
+import java.time.Duration;
+import java.time.Instant;
+
 public class Main {
     public static void main(String[] args) {
-        MatrixMultiplication m = new MatrixMultiplication(1024, 6834723);
-        m.multiplySequential();
-//        m.multiplyParallel(8);
+        MatrixMultiplication m = new MatrixMultiplication(1024, 6834723, 8);
+        Instant start = Instant.now();
+//        m.multiplySequential();
+        Instant end = Instant.now();
+        Duration elapsedTime = Duration.between(start, end);
+        System.out.printf("Sequential %d ms\n", elapsedTime.toMillis());
+
+        m.resetResultMatrix();
+        start = Instant.now();
+        m.multiplyParallel();
+        end = Instant.now();
+        elapsedTime = Duration.between(start, end);
+        System.out.printf("Parallel %d ms\n", elapsedTime.toMillis());
+
+        m.resetResultMatrix();
+        start = Instant.now();
+        m.multiplyForkJoin();
+        end = Instant.now();
+        elapsedTime = Duration.between(start, end);
+        System.out.printf("ForkJoin Rec %d ms\n", elapsedTime.toMillis());
+
+        m.resetResultMatrix();
+        start = Instant.now();
+        m.multiplyForkJoinIterative();
+        end = Instant.now();
+        elapsedTime = Duration.between(start, end);
+        System.out.printf("ForkJoin Iter %d ms\n", elapsedTime.toMillis());
     }
 }
--- src/main/java/matrix/MatrixMultiplication.java
+++ src/main/java/matrix/MatrixMultiplication.java
@@ -1,40 +1,47 @@
 package matrix;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 
 public class MatrixMultiplication {
     private final int size;
+    private final int numThreads;
     private final Random rand;
     private final double[][] left;
     private final double[][] right;
     private final double[][] result;
 
-    public MatrixMultiplication(int size, int seed) {
+    public MatrixMultiplication(int size, int seed, int numThreads) {
         this.size = size;
+        this.numThreads = numThreads;
         left = new double[size][size];
         right = new double[size][size];
         result = new double[size][size];
         rand = new Random(seed);
+        for (int rowIdx = 0; rowIdx < size; rowIdx++) {
+            for (int colIdx = 0; colIdx < size; colIdx++) {
+                left[rowIdx][colIdx] = rand.nextDouble();
+                right[rowIdx][colIdx] = rand.nextDouble();
+                result[rowIdx][colIdx] = 0;
+            }
+        }
     }
 
     public void multiplySequential() {
-        initializeAllMatrix();
-
         multiplyRowInRange(0, size);
 
         System.out.println(result[0][0]);
     }
 
-    public void multiplyParallel(int numThreads) {
+    public void multiplyParallel() {
         if (numThreads > size) {
             multiplySequential();
             return;
         }
-
-        initializeAllMatrix();
 
         ExecutorService executor = Executors.newFixedThreadPool(numThreads);
         int rowsPerThread = size / numThreads;
@@ -42,23 +49,87 @@ public class MatrixMultiplication {
 
         int start = 0;
         for (int i = 0; i < numThreads; i++) {
-            int end = start + rowsPerThread + (i < remainder ? 1 : 0); // hand remainder
+            int end = start + rowsPerThread + (i < remainder ? 1 : 0); // handle remainder
+            // Borrar comentario: LA idea es que por cada tarea que sobre, se da una a cada thread hasta que no queden
             int finalStart = start;
             executor.submit(() -> multiplyRowInRange(finalStart, end));
             start = end;
         }
 
-        executor.shutdown();
-        try {
-            if (!executor.awaitTermination(1, TimeUnit.MINUTES)) {
-                executor.shutdownNow();
-            }
-        } catch (InterruptedException e) {
-            executor.shutdownNow();
-            Thread.currentThread().interrupt();
-        }
+        shutdownExecutor(executor);
 
         System.out.println(result[0][0]);
+    }
+
+    public void multiplyForkJoin() {
+        ForkJoinPool pool = new ForkJoinPool(numThreads);
+        pool.invoke(new MultiplyTask(0, size, Math.max(20, size/numThreads)));
+
+        shutdownExecutor(pool);
+
+        System.out.println(result[0][0]);
+    }
+
+    public void multiplyForkJoinIterative() {
+        if (numThreads > size) {
+            multiplySequential();
+            return;
+        }
+
+        int rowsPerThread = size / numThreads;
+        int remainder = size % numThreads;
+
+        List<MultiplyTaskIterative> tasks = new ArrayList<>();
+
+        int start = 0;
+        for (int i = 0; i < numThreads; i++) {
+            int end = start + rowsPerThread + (i < remainder ? 1 : 0);
+            tasks.add(new MultiplyTaskIterative(start, end));
+            start = end;
+        }
+
+        ForkJoinPool pool = new ForkJoinPool(numThreads);
+        pool.submit(() -> ForkJoinTask.invokeAll(tasks));
+        shutdownExecutor(pool);
+
+        System.out.println(result[0][0]);
+    }
+
+    private class MultiplyTask extends RecursiveAction {
+        private final int startRow;
+        private final int endRow;
+        private final int threshold;
+
+        MultiplyTask(int startRow, int endRow, int threshold) {
+            this.startRow = startRow;
+            this.endRow = endRow;
+            this.threshold = threshold;
+        }
+
+        @Override
+        protected void compute() {
+            if (endRow - startRow <= threshold) {
+                multiplyRowInRange(startRow, endRow);
+                return;
+            }
+            int middleRow = (startRow + endRow) / 2;
+            invokeAll(new MultiplyTask(startRow, middleRow, threshold), new MultiplyTask(middleRow, endRow, threshold));
+        }
+    }
+
+    private class MultiplyTaskIterative extends RecursiveAction {
+        private final int startRow;
+        private final int endRow;
+
+        MultiplyTaskIterative(int startRow, int endRow) {
+            this.startRow = startRow;
+            this.endRow = endRow;
+        }
+
+        @Override
+        protected void compute() {
+            multiplyRowInRange(startRow, endRow);
+        }
     }
 
     // endIdx exclusive
@@ -72,13 +143,23 @@ public class MatrixMultiplication {
         }
     }
 
-    private void initializeAllMatrix() {
+    public void resetResultMatrix() {
         for (int rowIdx = 0; rowIdx < size; rowIdx++) {
             for (int colIdx = 0; colIdx < size; colIdx++) {
-                left[rowIdx][colIdx] = rand.nextDouble();
-                right[rowIdx][colIdx] = rand.nextDouble();
                 result[rowIdx][colIdx] = 0;
             }
+        }
+    }
+
+    private void shutdownExecutor(ExecutorService executor) {
+        executor.shutdown();
+        try {
+            if (!executor.awaitTermination(1, TimeUnit.MINUTES)) {
+                executor.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            executor.shutdownNow();
+            Thread.currentThread().interrupt();
         }
     }
 }
